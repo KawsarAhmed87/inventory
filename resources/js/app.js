8ff184e2f3f81fcd9cb8bba573f71eefab454eac
KawsarAhmed87 <ahmedkawsarbd@gmail.com>
2022-01-13 13:35:31 +0600

    register, login page  create

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,19 +6,17 @@ require('./bootstrap');
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
+import {routes} from './routes';
+
+const router = new VueRouter({
+    routes, mode: 'history'
+  })
 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app',
+    router
 });
